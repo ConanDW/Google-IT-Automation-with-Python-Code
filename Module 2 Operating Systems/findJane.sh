cc720cdd4d103ac097ea4fc4fb7dbbaaad0b523b
Cameron Day <32853335+ConanDW@users.noreply.github.com>
2024-11-11 15:18:35 -0500

    Delete Module 2 Operating Systems/findJane.sh

--- Module 2 Operating Systems/findJane.sh
+++
@@ -1,6 +0,0 @@
-files=$(grep " jane " ~/data/list.txt | cut -d " " -f 3)
-for file in $files; do 
-        if test -e ~/$file; then echo $file >> oldFiles.txt; else echo "file not found"; fi
-done
-
-#REMEBER TO GO BACK AND SAVE OLD LABS
