ff25f9a09fd7ef8ea5ad3c96c3f6526f72951728
Cameron Day <32853335+ConanDW@users.noreply.github.com>
2024-11-11 15:19:37 -0500

    Delete Module 2 Operating Systems/filepaths.py

--- Module 2 Operating Systems/filepaths.py
+++
@@ -1,7 +0,0 @@
-import os 
-#Windows file directory written in Python
-"C:/my-directory/target-file.txt."
-#CWD command: 
-os.getcwd()
-#CWD command for external files:
-outputs['current_directory_before'] = os.getcwd()
